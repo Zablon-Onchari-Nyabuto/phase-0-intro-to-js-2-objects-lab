dd503b088552847ec565de0281c8125346a730e5
Zablon-Onchari-Nyabuto <zablon.nyabuto@student.moringaschool.com>
2022-08-21 11:25:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+const employee = { name: 'Zablon', streetAddress: '50504 Makongeni' };
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    let newEmployee = { ...employee };
+    newEmployee.name = 'Sam';
+    newEmployee.streetAddress = '11 Broadway';
+    return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee.streetAddress = '12 Broadway';
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = { ...employee }
+    delete newEmployee.name;
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    let newEmployee = delete employee.name;
+    return employee;
+}
